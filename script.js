51c70b03636b7d37650447a24c5098b20308fc21
PraveenAkashk <praveenkadhiravan52853@gmail.com>
2023-06-24 17:52:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+let Slider = document.querySelectorAll(".slider");
+let leftButton = document.querySelector(".left-button");
+let rightButton = document.querySelector(".right-button");
+let slidercontainer=document.querySelector(".slider-main");
+let singleslider= document.querySelector(".slider")
+
+console.log("leftButton", leftButton)
+console.log("rightButton", rightButton)
+
+let sliderlenght= Slider.length;
+
+let count= 0;
+
+if(count<=0){
+    leftButton.classList.add("disbale")
+}
+
+console.log("count",count)
+
+rightButton.addEventListener("click", ()=>{
+    count++;
+    if(count < sliderlenght){
+        removeclass();
+        currentslide();
+
+        if(count>= sliderlenght-1){
+            rightButton.classList.add("disbale")
+        }
+
+    }
+    if(count>0){
+        leftButton.classList.remove("disbale")
+    }
+})
+
+
+leftButton.addEventListener("click",()=>{
+    count--;
+
+    if(count < sliderlenght){
+        removeclass();
+        currentslide();
+    }
+    
+    if(count<= sliderlenght-2){
+        rightButton.classList.remove("disbale")
+    }
+
+    if(count<=0){
+        leftButton.classList.add("disbale")
+    }
+
+})
+
+
+
+// Current Slide
+let currentslide=()=>{
+    Slider[count].classList.add("active")
+    slidercontainer.style.transition="0.5s linear"
+    slidercontainer.style.marginLeft =`-${count*(singleslider.clientWidth)}px`;
+}
+
+// To remove active class
+let removeclass=()=>{
+    let perviousactiveelem= document.querySelectorAll(".slider.active")
+    for(let prevelem of perviousactiveelem)
+    {
+        prevelem.classList.remove("active");
+    }
+}
+
+
+
+
+
+
+
+
